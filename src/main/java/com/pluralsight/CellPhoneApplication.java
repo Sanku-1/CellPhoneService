b2edb41555fa1a08f5b352d5be68573b4b083e0f
Stephen_A <ankustephen99@gmail.com>
2025-10-01 10:28:29 -0400

    Added user prompts for setters

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,7 +1,37 @@
 package com.pluralsight;
 
+import java.awt.*;
+import java.util.Scanner;
+
 public class CellPhoneApplication {
     public static void main(String[] args) {
+        CellPhone cellPhone1 = new CellPhone();
+        Scanner cellScanner = new Scanner(System.in);
+
+        System.out.println("What is the serial number?:");
+        cellPhone1.setSerialNumber(cellScanner.nextInt());
+
+        cellScanner.nextLine();
+
+        System.out.println("What model is the phone?:");
+        cellPhone1.setModel(cellScanner.nextLine());
+
+        System.out.println("Who is the carrier?:");
+        cellPhone1.setCarrier(cellScanner.nextLine());
+
+        System.out.println("What is the phone number?:");
+        cellPhone1.setPhoneNumber(cellScanner.nextLine());
+
+        System.out.println("Who is the owner of the phone?:");
+        cellPhone1.setOwner(cellScanner.nextLine());
+
+
+        System.out.println("Serial Number: " + cellPhone1.getSerialNumber());
+        System.out.println("Model: " + cellPhone1.getModel());
+        System.out.println("Carrier: " + cellPhone1.getCarrier());
+        System.out.println("Phone Number: " + cellPhone1.getPhoneNumber());
+        System.out.println("Owner: " + cellPhone1.getOwner());
+
 
     }
 }
